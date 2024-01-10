979f63a5ffb82f0792729be18e3b1e009c9d2185
Am <arphasirisubb@gmail.com>
2024-01-10 12:41:45 +0700

    edit(ex-1):correct answer 3

--- exercise-1.js
+++ exercise-1.js
@@ -6,4 +6,6 @@ db.pizzaOrders.findOne({
 //- ใช้ `find` ในการหาข้อมูล Document ทั้งหมดที่จ่ายเงินด้วย Credit Card ของ `mastercard`
 //- ใช้ `find` ในการหาข้อมูล Document ทั้งหมดที่สั่ง Pizza ขนาด `medium` โดย Limit จำนวนข้อมูลแค่ 5 Documents
 
-db.pizzaOrders.find({ credit_card_type: "mastercard", size: "medium" });
+db.pizzaOrders.find({ credit_card_type: "mastercard" });
+
+db.pizzaOrders.find({ size: "medium" });
